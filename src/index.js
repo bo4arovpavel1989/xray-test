b897219e461a01b7e7088a2ae55d8a0a5cdd9ade
User <bo4arovpavel1989@mail.ru>
2019-01-11 09:16:42 +0300

    inited basics of react app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+
+class App extends React.Component {
+
+  constructor(){
+    super();
+
+  }
+
+  render(){
+    return null;
+  }
+}
+
+ReactDom.render(
+  <App/>,
+  document.getElementById('root')
+)
