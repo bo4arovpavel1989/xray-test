df52b25dcf5f63baa91114370eb1f711ba82ca44
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2019-03-27 20:09:46 +0300

    configured jest. can stat testing

--- __tests__/components/Header.js
+++ __tests__/components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from '../../src/components/Header';
-import { Enzyme, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('renders correctly', () => {
-  const output = shallow(<Header/>);
+  const output = shallow(
+    <Header
+      isAdmin = { false }
+      authFail = { false }
+    />);
 
   expect(shallowToJson(output)).toMatchSnapshot();
 })
---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "moduleNameMapper": {
+      "^.+\\.(css|less|sass)$": "identity-obj-proxy"
+    }
+};
