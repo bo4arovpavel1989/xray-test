21b8c9cc126788568a6aadbf043f269510184deb
User <bo4arovpavel1989@mail.ru>
2019-03-28 12:01:20 +0300

    made first test for header component

--- __tests__/components/Header.js
+++ __tests__/components/Header.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import Header from '../../src/components/Header';
-import Enzyme, { shallow } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-it('renders correctly', () => {
-  const output = shallow(
-    <Header
-      isAdmin = { false }
-      authFail = { false }
-    />);
+describe('Header component', () => {
+  const props = {
+    isAdmin: false,
+    authFail: false,
+    login () {},
+    logoff () {}
+  };
 
-  expect(shallowToJson(output)).toMatchSnapshot();
+  describe('Header initial', () => {
+    const header = mount(
+      <BrowserRouter>
+        <Header { ...props }/>
+      </BrowserRouter>
+    );
+
+    it('should render h1', () => expect(header.find('h1').text()).toEqual('XRay-test'))
+    it('should render Login component', () => expect(header.find('Login')).toHaveLength(1))
+  })
 })
