027937f5349a99a6e111fc9adddee68a6073e42a
User <bo4arovpavel1989@mail.ru>
2019-03-29 09:44:29 +0300

    made tests for Header component

--- __tests__/components/Header.js
+++ __tests__/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import Header from '../../src/components/Header';
 import Enzyme, { mount } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -14,6 +15,16 @@ describe('Header component', () => {
     logoff () {}
   };
 
+  describe('Header snapshot', () => {
+    const header = mount(
+      <BrowserRouter>
+        <Header { ...props }/>
+      </BrowserRouter>
+    );
+
+    it('should fit snapshot', () => expect(shallowToJson(header)).toMatchSnapshot())
+  })
+
   describe('Header initial', () => {
     const header = mount(
       <BrowserRouter>
@@ -23,5 +34,24 @@ describe('Header component', () => {
 
     it('should render h1', () => expect(header.find('h1').text()).toEqual('XRay-test'))
     it('should render Login component', () => expect(header.find('Login')).toHaveLength(1))
+  })
+
+  describe('Header logged in', () => {
+    const mockLogoff = jest.fn();
+
+    props.isAdmin = true;
+    props.logoff = mockLogoff;
+
+    const header = mount(
+      <BrowserRouter>
+        <Header { ...props }/>
+      </BrowserRouter>
+    );
+
+    it('should fit snapshot', () => expect(shallowToJson(header)).toMatchSnapshot())
+    it('should call logoff fn when clicking on logoff link', () => {
+      header.find('.logoff').simulate('click');
+      expect(mockLogoff).toHaveBeenCalledTimes(1);
+    });
   })
 })
